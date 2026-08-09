84675b097e99f098c554224f5337c83f2e44a034
kanakotswaraj-lang <kanakotswaraj@gmail.com>
2026-08-09 13:37:32 +0530

    Remove firebase configuration from app.js
    Removed firebaseConfig object from app.js.

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAJw47JuihhalCFvFAyrX_beM0iZZ0XltA",
-    authDomain: "mesthri-app.firebaseapp.com",
-    databaseURL: "https://mesthri-app-default-rtdb.firebaseio.com",
-    projectId: "mesthri-app",
-    storageBucket: "mesthri-app.firebasestorage.app",
-    messagingSenderId: "1060617901864",
-    appId: "1:1060617901864:web:5ffb9378cf36bd6d35221d",
-    measurementId: "G-J9Y5TW9D7T"
-};
-
+const 
 // ഫയർബേസ് ഇനിഷ്യലൈസ് ചെയ്യുക
 const app = initializeApp(firebaseConfig);
 
